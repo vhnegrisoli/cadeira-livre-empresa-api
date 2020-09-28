8627af9ecbff5042e92575c952e3fde995d61f74
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2020-09-28 18:05:29 -0300

    Inicia testes de integração e unitários na service de usuários

---
+++ src/test/java/br/com/cadeiralivreempresaapi/modulos/usuario/service/UsuarioServiceIntegrationTest.java
@@ -0,0 +1,205 @@
+package br.com.cadeiralivreempresaapi.modulos.usuario.service;
+
+import br.com.cadeiralivreempresaapi.config.exception.ValidacaoException;
+import br.com.cadeiralivreempresaapi.modulos.empresa.service.EmpresaService;
+import br.com.cadeiralivreempresaapi.modulos.funcionario.service.FuncionarioService;
+import br.com.cadeiralivreempresaapi.modulos.usuario.enums.ESituacaoUsuario;
+import br.com.cadeiralivreempresaapi.modulos.usuario.repository.UsuarioRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static br.com.cadeiralivreempresaapi.modulos.usuario.mocks.UsuarioMocks.umUsuarioAutenticadoAdmin;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@Import(UsuarioService.class)
+@Sql(scripts = {"classpath:/usuarios_tests.sql"})
+@DataJpaTest
+public class UsuarioServiceIntegrationTest {
+
+    @Autowired
+    private UsuarioService service;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+    @MockBean
+    private AutenticacaoService autenticacaoService;
+    @MockBean
+    private PermissaoService permissaoService;
+    @MockBean
+    private EmpresaService empresaService;
+    @MockBean
+    private FuncionarioService funcionarioService;
+
+    @Test
+    public void buscarPorId_deveBuscarUsuario_quandoEncontrarPorId() {
+        var usuario = service.buscarPorId(1);
+
+        assertThat(usuario).isNotNull();
+        assertThat(usuario.getNome()).isEqualTo("Victor Hugo Negrisoli");
+        assertThat(usuario.getEmail()).isEqualTo("victorhugonegrisoli.ccs@gmail.com");
+    }
+
+    @Test
+    public void buscarPorId_deveLancarException_quandoNaoEncontrarPorId() {
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.buscarPorId(1000))
+            .withMessage("Usuário não encontrado.");
+    }
+
+    @Test
+    public void getUsuarioAutenticadoAtualizaUltimaData_deveRetornarUsuarioAutenticadoEAtualizarUltimoAcesso_quandoSolicitado() {
+        when(autenticacaoService.getUsuarioAutenticadoId()).thenReturn(1);
+        var usuarioUltimoAcessoAnterior = usuarioRepository.findById(1).get().getUltimoAcesso();
+        var response = service.getUsuarioAutenticadoAtualizaUltimaData();
+        assertThat(response).isNotNull();
+        assertThat(response.getUltimoAcesso()).isNotEqualTo(usuarioUltimoAcessoAnterior);
+    }
+
+    @Test
+    public void getUsuarioAutenticadoAtualizaUltimaData_deveLancarException_quandoNaoEncontrarUsuario() {
+        when(autenticacaoService.getUsuarioAutenticadoId()).thenReturn(10000);
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.getUsuarioAutenticadoAtualizaUltimaData())
+            .withMessage("Usuário não encontrado.");
+    }
+
+    @Test
+    public void atualizarTokenNotificacao_deveAtualizarToken_quandoUsuarioNaoPossuirToken() {
+        when(autenticacaoService.getUsuarioAutenticadoId()).thenReturn(2);
+
+        var usuarioTokenAntigo = usuarioRepository.findById(2).get();
+        assertThat(usuarioTokenAntigo.getTokenNotificacao()).isNull();
+
+        var response = service.atualizarTokenNotificacao("123456789");
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("Token de notificação atualizado com sucesso!");
+
+        var usuarioTokenNovo = usuarioRepository.findById(2).get();
+        assertThat(usuarioTokenNovo.getTokenNotificacao()).isEqualTo("123456789");
+    }
+
+    @Test
+    public void atualizarTokenNotificacao_deveAtualizarToken_quandoInformarUmNovoQueOUsuarioNaoPossui() {
+        when(autenticacaoService.getUsuarioAutenticadoId()).thenReturn(1);
+
+        var usuarioTokenAntigo = usuarioRepository.findById(1).get();
+        assertThat(usuarioTokenAntigo.getTokenNotificacao()).isEqualTo("123456");
+
+        var response = service.atualizarTokenNotificacao("123456789");
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("Token de notificação atualizado com sucesso!");
+
+        var usuarioTokenNovo = usuarioRepository.findById(1).get();
+        assertThat(usuarioTokenNovo.getTokenNotificacao()).isEqualTo("123456789");
+    }
+
+    @Test
+    public void atualizarTokenNotificacao_deveNaoAtualizarToken_quandoInformarUmTokenQueUsuarioJaPossui() {
+        when(autenticacaoService.getUsuarioAutenticadoId()).thenReturn(1);
+
+        var usuarioTokenAntigo = usuarioRepository.findById(1).get();
+        assertThat(usuarioTokenAntigo.getTokenNotificacao()).isEqualTo("123456");
+
+        var response = service.atualizarTokenNotificacao("123456");
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("O usuário já possui esse token de notificação.");
+
+        var usuarioTokenNovo = usuarioRepository.findById(1).get();
+        assertThat(usuarioTokenNovo.getTokenNotificacao()).isEqualTo("123456");
+    }
+
+    @Test
+    public void atualizarTokenNotificacao_deveLancarException_quandoNaoEncontrarUsuario() {
+        when(autenticacaoService.getUsuarioAutenticadoId()).thenReturn(10000);
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.atualizarTokenNotificacao("123456789"))
+            .withMessage("Usuário não encontrado.");
+    }
+
+    @Test
+    public void alterarSituacao_deveAtivar_quandoUsuarioEstiverInativoEForAdmin() {
+        when(autenticacaoService.getUsuarioAutenticado()).thenReturn(umUsuarioAutenticadoAdmin());
+
+        alterarSituacao(1, ESituacaoUsuario.INATIVO);
+
+        var response = service.alterarSituacao(1);
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("A situação do usuário foi alterada com sucesso!");
+
+        var usuarioAlterado = usuarioRepository.findById(1).get();
+        assertThat(usuarioAlterado.isAtivo()).isTrue();
+
+        verify(funcionarioService, times(0)).validarUsuario(anyInt());
+    }
+
+    @Test
+    public void alterarSituacao_deveAtivar_quandoUsuarioEstiverInativoEForProprietario() {
+
+    }
+
+    @Test
+    public void alterarSituacao_deveAtivar_quandoUsuarioEstiverInativoEForSocio() {
+
+    }
+
+    @Test
+    public void alterarSituacao_deveAtivar_quandoUsuarioEstiverInativoEForFuncionario() {
+
+    }
+
+    @Test
+    public void alterarSituacao_deveInativar_quandoUsuarioEstiverAtivoEForAdmin() {
+        when(autenticacaoService.getUsuarioAutenticado()).thenReturn(umUsuarioAutenticadoAdmin());
+
+        alterarSituacao(1, ESituacaoUsuario.ATIVO);
+
+        var response = service.alterarSituacao(1);
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("A situação do usuário foi alterada com sucesso!");
+
+        var usuarioAlterado = usuarioRepository.findById(1).get();
+        assertThat(usuarioAlterado.isAtivo()).isFalse();
+
+        verify(funcionarioService, times(0)).validarUsuario(anyInt());
+    }
+
+    @Test
+    public void alterarSituacao_deveInativar_quandoUsuarioEstiverAtivoEForProprietario() {
+
+    }
+
+    @Test
+    public void alterarSituacao_deveInativar_quandoUsuarioEstiverAtivoEForSocio() {
+
+    }
+
+    @Test
+    public void alterarSituacao_deveInativar_quandoUsuarioEstiverAtivoEForFuncionario() {
+
+    }
+
+    private void alterarSituacao(Integer id, ESituacaoUsuario situacao) {
+        var usuario = usuarioRepository.findById(id).get();
+        usuario.setSituacao(situacao);
+        usuarioRepository.save(usuario);
+    }
+}
---
+++ src/test/java/br/com/cadeiralivreempresaapi/modulos/usuario/service/UsuarioServiceTest.java
@@ -0,0 +1,258 @@
+package br.com.cadeiralivreempresaapi.modulos.usuario.service;
+
+import br.com.cadeiralivreempresaapi.config.exception.PermissaoException;
+import br.com.cadeiralivreempresaapi.config.exception.ValidacaoException;
+import br.com.cadeiralivreempresaapi.modulos.empresa.service.EmpresaService;
+import br.com.cadeiralivreempresaapi.modulos.funcionario.service.FuncionarioService;
+import br.com.cadeiralivreempresaapi.modulos.usuario.model.Usuario;
+import br.com.cadeiralivreempresaapi.modulos.usuario.repository.UsuarioRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static br.com.cadeiralivreempresaapi.modulos.usuario.mocks.UsuarioMocks.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class UsuarioServiceTest {
+
+    @InjectMocks
+    private UsuarioService service;
+    @Mock
+    private UsuarioRepository usuarioRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private AutenticacaoService autenticacaoService;
+    @Mock
+    private PermissaoService permissaoService;
+    @Mock
+    private EmpresaService empresaService;
+    @Mock
+    private FuncionarioService funcionarioService;
+
+    @Test
+    public void salvarProprietario_deveSalvarUsuarioProprietario_quandoDadosEstiveremCorretos() {
+        when(permissaoService.buscarPorCodigo(any())).thenReturn(umaPermissaoProprietario());
+
+        var request = umUsuarioRequest();
+        var response = service.salvarProprietario(request);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("Proprietário inserido com sucesso!");
+
+        verify(passwordEncoder, times(1)).encode(anyString());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarSocio_deveSalvarUsuarioSocio_quandoDadosEstiveremCorretos() {
+        when(permissaoService.buscarPorCodigo(any())).thenReturn(umaPermissaoSocio());
+        when(usuarioRepository.save(any(Usuario.class))).thenReturn(umUsuario());
+
+        var request = umUsuarioRequest();
+        var response = service.salvarSocio(request, 1);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("Sócio inserido com sucesso!");
+
+        verify(passwordEncoder, times(1)).encode(anyString());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+        verify(empresaService, times(1)).inserirSocio(any(Usuario.class), anyInt());
+    }
+
+    @Test
+    public void salvarFuncionario_deveSalvarUsuarioFuncionario_quandoDadosEstiveremCorretos() {
+        when(permissaoService.buscarPorCodigo(any())).thenReturn(umaPermissaoFuncionario());
+        when(usuarioRepository.save(any(Usuario.class))).thenReturn(umUsuario());
+
+        var request = umUsuarioRequest();
+        var response = service.salvarFuncionario(request, 1);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("Funcionário inserido com sucesso!");
+
+        verify(passwordEncoder, times(1)).encode(anyString());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+        verify(funcionarioService, times(1)).salvarFuncionario(any(Usuario.class), anyInt());
+    }
+
+    @Test
+    public void salvarUsuario_deveSalvarUsuario_quandoDadosEstiveremCorretos() {
+        when(usuarioRepository.save(any(Usuario.class))).thenReturn(umUsuario());
+
+        var usuarioSalvo = service.salvarUsuario(umUsuario());
+
+        assertThat(usuarioSalvo).isNotNull();
+
+        verify(passwordEncoder, times(1)).encode(anyString());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoCpfForInvalido() {
+        var usuario = umUsuario();
+        usuario.setCpf("103.324.589-53");
+
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("O CPF está inválido.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoCpfForNulo() {
+        var usuario = umUsuario();
+        usuario.setCpf(null);
+
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("O CPF deve ser informado.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoDataNascimentoForIgualHoje() {
+        var usuario = umUsuario();
+        usuario.setDataNascimento(LocalDate.now());
+
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("A data de nascimento não pode ser igual à data de hoje.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoDataNascimentoForSuperiorHoje() {
+        var usuario = umUsuario();
+        usuario.setDataNascimento(LocalDate.now().plusDays(1));
+
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("A data de nascimento não pode ser superior à data de hoje.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoEmailJaExistirSalvoParaOutroId() {
+        when(usuarioRepository.findByEmail(anyString())).thenReturn(Optional.of(umUsuario()));
+
+        var usuario = umUsuario();
+        usuario.setId(123);
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("Email já cadastrado para um usuário ativo.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoEmailJaExistir() {
+        when(usuarioRepository.findByEmail(anyString())).thenReturn(Optional.of(umUsuario()));
+
+        var usuario = umUsuario();
+        usuario.setId(null);
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("Email já cadastrado para um usuário ativo.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoCpfJaExistirSalvoParaOutroId() {
+        when(usuarioRepository.findByCpf(anyString())).thenReturn(Optional.of(umUsuario()));
+
+        var usuario = umUsuario();
+        usuario.setId(123);
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("CPF já cadastrado para um usuário ativo.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuario_deveLancarException_quandoCpfJaExistir() {
+        when(usuarioRepository.findByCpf(anyString())).thenReturn(Optional.of(umUsuario()));
+
+        var usuario = umUsuario();
+        usuario.setId(null);
+        assertThatExceptionOfType(ValidacaoException.class)
+            .isThrownBy(() -> service.salvarUsuario(usuario))
+            .withMessage("CPF já cadastrado para um usuário ativo.");
+
+        verify(passwordEncoder, times(0)).encode(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void editarDadosUsuario_deveEditarDados_quandoDadosCorretosComPermissao() {
+        when(autenticacaoService.getUsuarioAutenticado()).thenReturn(umUsuarioAutenticadoAdmin());
+        when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(umUsuario()));
+
+        var request = umUsuarioRequest();
+        var response = service.editarDadosUsuario(request, 1);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("Usuário alterado com sucesso!");
+
+        verify(usuarioRepository, times(1)).findById(anyInt());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void editarDadosUsuario_deveEditarDados_quandoDadosCorretosSendoAdmin() {
+        when(autenticacaoService.getUsuarioAutenticado()).thenReturn(umUsuarioAutenticadoAdmin());
+        when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(umUsuario()));
+
+        var request = umUsuarioRequest();
+        var response = service.editarDadosUsuario(request, 1);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getMessage()).isEqualTo("Usuário alterado com sucesso!");
+
+        verify(usuarioRepository, times(1)).findById(anyInt());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void editarDadosUsuario_deveLancarException_quandoUsuarioNaoTiverPermissaoParaAlterarDados() {
+        when(autenticacaoService.getUsuarioAutenticado()).thenReturn(umUsuarioAutenticadoFuncionario());
+        when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(umUsuario()));
+
+        var request = umUsuarioRequest();
+        assertThatExceptionOfType(PermissaoException.class)
+            .isThrownBy(() -> service.editarDadosUsuario(request, 10000))
+            .withMessage("Você não possui permissão para alterar os dados desse usuário.");
+
+        verify(usuarioRepository, times(0)).findById(anyInt());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
+    }
+}
